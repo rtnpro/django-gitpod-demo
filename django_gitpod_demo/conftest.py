b1c7173f7828cc04e62d1a8ecb4d06ce848699a2
Ratnadeep Debnath <rtnpro@gmail.com>
2021-11-02 21:28:48 +0530

    Initialize repo from django-cookiecutter template

---
+++ django_gitpod_demo/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from django_gitpod_demo.users.models import User
+from django_gitpod_demo.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
