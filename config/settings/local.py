0cdd7ce0108ac18191ba1322fc2dfd68c94aa7f1
Ratnadeep Debnath <rtnpro@gmail.com>
2021-11-03 12:13:19 +0000

    Allow all hosts in ALLOWED_HOSTS for local development.

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="ISfdVax75963IkB7Z3QHVaHdIsaa6PX6FSRKt9Q6eYxAZhxy2WPRa41LFqprEQeV",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 # CACHES
 # ------------------------------------------------------------------------------
